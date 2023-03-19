191131a4dc824d13feb0c052d41baa7f77e4e673
LACNIQ <125921627+LACNIQ@users.noreply.github.com>
2023-03-19 18:22:14 +0530

    Update ps.py

--- ps.py
+++ ps.py
@@ -38,7 +38,7 @@ if input1 and output is not None:
         return dff.to_csv().encode('utf-8')
     csv = convert_df(test)
     st.download_button(
-        label="Download file",data=Path("output.csv").read_text()
+        label="Download file",data=Path("output.csv").read_text(),
         data=csv,
         file_name='output.csv',
         mime='text/csv',)
